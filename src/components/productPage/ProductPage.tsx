3b15d58bf8a1941e0fb9dc9334b68f19e1ab5f4f
atapel <a.tapel@vitrina.tv>
2023-03-26 16:08:09 +0300

    controls added

--- src/components/productPage/ProductPage.tsx
+++ src/components/productPage/ProductPage.tsx
@@ -1,8 +1,12 @@
 import './ProductPage.css';
 import {Product} from "../../models/Product";
 import { ImagesGallery } from "../images-gallery/ImagesGallery";
+import { useState } from 'react';
 
 export const ProductPage: React.FC<{product: Product}> = ({product}) => {
+    
+    const [count, setCount] = useState<number>(0);
+
     const getProductPrice = (): string => {
         if (product.discountPercent) {
             return `$${Math.floor(product.price * product.discountPercent / 100)}.00`
@@ -10,17 +14,39 @@ export const ProductPage: React.FC<{product: Product}> = ({product}) => {
         return `$${Math.floor(product.price)}.00`;
     }
     
+    const addProductToCart = (): void => {
+        
+    }
+
+    const incrementCount = (): void => {
+        setCount(count + 1);
+    }
+
+    const decrementCount = (): void => {
+        setCount(count - 1);
+    }
+
     return(
         <div className="product-page-container">
             <div className="carrousel-block">
                 <ImagesGallery images={product.images}/>
             </div>
-            <div className="product-info-block">
+            <aside className="product-info-block">
                 <span className="product-brand">{product.brand.toUpperCase()}</span>
                 <h2 className="product-name">{product.name}</h2>
                 <p className="product-description">{product.description}</p>
                 <p className="product-price">{getProductPrice()}</p>
-            </div>
+                <div className="controls-block">
+                    <div className="quantity-control-block">
+                        <button disabled={count <= 0} onClick={decrementCount}>
+                            <img alt="Minus" src='/images/icon-minus.svg'/>
+                        </button>
+                        <span>{count}</span>
+                        <button onClick={incrementCount}><img alt="Plus" src='/images/icon-plus.svg'/></button>
+                    </div>
+                    <button onClick={addProductToCart} className="add-to-cart-btn">Add to cart</button>
+                </div>
+            </aside>
         </div>
     )
 }
